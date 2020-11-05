1a199f4a741a8ed32599eea089f085e3aec3c444
Holly Chaffee <hollydchaffee@gmail.com>
2020-11-05 13:27:51 -0500

    Triangle Utilities Test Working

--- src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
@@ -3,18 +3,45 @@ package com.returnready.loopthereitis;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = new String();
+        for(int row = 0; row < numberOfRows-1; row++) {
+            for(int col = 0; col <= row; col++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = new String();
+        for(int i = 0; i < numberOfStars; i++ ){
+            row += "*";
+        }
+        return row;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = new String();
+        for(int row = 0; row < 4; row++) {
+            for(int col = 0; col <= row; col++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String triangle = new String();
+        for(int row = 0; row < 9; row++) {
+            for(int col = 0; col <= row; col++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
-}
+
+    }
+
