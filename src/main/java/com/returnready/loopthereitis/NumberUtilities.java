026c45211ed6ca722ed8e3ed1d87ce52b37bc8ae
Holly Chaffee <saltdc95@gmail.com>
2020-11-13 18:34:47 -0500

    Number utilities updated

--- src/main/java/com/returnready/loopthereitis/NumberUtilities.java
+++ src/main/java/com/returnready/loopthereitis/NumberUtilities.java
@@ -2,49 +2,70 @@ package com.returnready.loopthereitis;
 
 
 public class NumberUtilities {
-    public static String getEvenNumbers(int start, int stop) {
 
-       return  "";
+    public static String getEvenNumbers(int start, int stop, int step) {
+        String evenNumbers = "";
+        for (int index = start; index < stop; index += step) {
+            if (index % 2 == 0) {
+                evenNumbers = evenNumbers + index;
+            }
+        }
+        return evenNumbers;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNumbers = "";
+        for (int index = start; index < stop; index++) {
+            if (index % 2 !=0) {
+                oddNumbers = oddNumbers + index;
+
+            }
+        }
+        return oddNumbers;
     }
 
-
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squareNumbers = "";
+        for (int index = start ; index < stop; index += step) {
+            squareNumbers += index * index;
+        }
+        return squareNumbers;
     }
 
     public static String getRange(int stop) {
-        String returnedRange = "";
-        for(int currentNumberInRange = 0; currentNumberInRange < stop - 1; currentNumberInRange++){
-            returnedRange = returnedRange + currentNumberInRange;
+        String Range1 = "";
+        for (int Index = 0; Index < stop - 1; Index++) {
+            Range1 = Range1 + Index;
         }
-        return returnedRange;
+        return Range1;
     }
 
     public static String getRange(int start, int stop) {
-        String returnedRange = "";
-        for(int currentNumberInRange = start; currentNumberInRange < stop; currentNumberInRange++){
-            returnedRange = returnedRange + currentNumberInRange;
+        String Range2 = "";
+        for (int index = start; index < stop; index++) {
+            Range2 = Range2 + index;
         }
-        return returnedRange;
+        return Range2;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        String returnedRange = "";
-        for(int currentNumberInRange = start; currentNumberInRange < stop; currentNumberInRange = currentNumberInRange + step){
-            returnedRange = returnedRange + currentNumberInRange;
+        String Range3 = "";
+        for (int index = start;index  < stop; index = index + step) {
+            Range3 = Range3 + index;
         }
-        return returnedRange;
+        return Range3;
 
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String Exponentiations = "";
+        for (int index = start; index < stop; index = index + step) {
+
+            Exponentiations = Exponentiations + (int) (Math.pow(index, exponent));
+        }
+        return Exponentiations;
     }
 }
+
--- src/test/java/com/returnready/loopthereitis/NumberUtilitiesTest.java
+++ src/test/java/com/returnready/loopthereitis/NumberUtilitiesTest.java
@@ -171,29 +171,29 @@ public class NumberUtilitiesTest {
 
 
     @Test
-    public void testGetEvenNumbers() {
+    public void testGetOddNumbers() {
         // : Given
         String expected = "5791113151719";
         int start = 5;
         int stop = 20;
 
         // : When
-        String actual = NumberUtilities.getEvenNumbers(start, stop);
+        String actual = NumberUtilities.getOddNumbers(start, stop);
 
         // : Then
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void testGetOddNumbers() {
+    public void testGetEvenNumbers() {
         // : Given
         String expected = "681012141618";
-        int start = 5;
-        int stop = 20;
-        int step = 5;
+        int start = 6;
+        int stop = 19;
+        int step = 2;
 
         // : When
-        String actual = NumberUtilities.getOddNumbers(start, stop);
+        String actual = NumberUtilities.getEvenNumbers(start, stop, step);
 
         // : Then
         Assert.assertEquals(expected, actual);
