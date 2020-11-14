6a106e4904a9d995fdfae4cdd0540e2be715cb3b
Holly Chaffee <saltdc95@gmail.com>
2020-11-14 16:19:40 -0500

    Table utilities working

--- src/main/java/com/returnready/loopthereitis/TableUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TableUtilities.java
@@ -2,14 +2,39 @@ package com.returnready.loopthereitis;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+
+        for (int r = 1; r <= 5; r++) {
+            for (int c = 1; c <= 5; c++) {
+              result = result + String.format("%3d |",c*r);
+            }
+            result += "\n";
+        }
+        return result;
     }
 
+
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+
+        for (int r = 1; r <= 10; r++) {
+            for (int c = 1; c <= 10; c++) {
+                result = result + String.format("%3d |",c*r);
+            }
+            result += "\n";
+        }
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+
+        for (int r = 1; r <= tableSize; r++) {
+            for (int c = 1; c <= tableSize; c++) {
+                result = result + String.format("%3d |",c*r);
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
